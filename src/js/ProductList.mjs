ab77c36e5956691bf492bc1743faf7b481c03f1c
tonbot <toy505050@yahoo.com>
2026-01-27 10:33:30 +0100

    new commit

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,4 +1,4 @@
-import { renderListWithTemplate } from './utils.mjs';
+import { renderListWithTemplate } from "./utils.mjs";
 
 // Template function for product cards
 function productCardTemplate(product) {
