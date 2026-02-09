7924f20245d9c514f868eeb55a063397d2980262
ander508 <oluchukwu1.offor@gmail.com>
2026-02-09 17:47:56 +0100

    wk04 team activity

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,30 +1,31 @@
-const baseURL = import.meta.env.VITE_SERVER_URL
-
+const baseURL = "https://wdd330-backend.onrender.com";
 
 function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
+  if (res.ok) return res.json();
+  throw new Error("Bad Response");
 }
 
 export default class ProductData {
-  constructor(category) {
-    // this.category = category;
-    // this.path = `/json/${this.category}.json`;
-  }
   async getData(category) {
-  const response = await fetch(`${baseURL}products/search/${category}`);
-    const data = await convertToJson(response);
-    console.log(data)
-    
-  return data.Result;
+    const res = await fetch(`${baseURL}/products/search/${category}`);
+    const data = await convertToJson(res);
+    return data.Result;
   }
-  
+
+  // GET single product by id
   async findProductById(id) {
-  const response = await fetch(`${baseURL}product/${id}`);
-  const data = await convertToJson(response);
-  return data.Result;
+    const res = await fetch(`${baseURL}/product/${id}`);
+    const data = await convertToJson(res);
+    return data.Result;
+  }
+
+  // POST checkout
+  async checkout(payload) {
+    const res = await fetch(`${baseURL}/checkout`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    });
+    return convertToJson(res);
   }
 }
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -9,7 +9,7 @@ const category = params.get("category");
 // console.log("Base URL:", import.meta.env.VITE_SERVER_URL);
 // console.log("Hello World")
 
-const dataSource = new ProductData("");
+const dataSource = new ProductData();
 
 const element = document.querySelector(".product-list");
 
